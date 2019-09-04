1a142f08d43a2bec164eedd7a870a26ce99e3176
Giuseppe Oliva <giuseppeoliva1997@gmail.com>
2019-09-04 15:31:24 +0200

    Inizializzazione progetto
    Creazione progetto.
    Inclusione frontend (app.js)

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+const $ = require("jquery");
+require("jquery-migrate");
+require("webpack-jquery-ui");
+require("bootstrap");
+require("../js/owl.carousel.min.js");
+require("../js/jquery.stellar.min.js");
+require("../js/jquery.countdown.min.js");
+require("../js/jquery.magnific-popup.min.js");
+require("aos");
+
+require("../js/main.js");
+
+require("../fonts/icomoon/style.css");
+
+require("../css/bootstrap.min.css");
+require("../css/magnific-popup.css");
+require("../css/bootstrap-datepicker.css");
+require("../fonts/flaticon/font/flaticon.css");
+require("../css/aos.css");
+
+require("../css/style.css");
+
+console.log('Hello Webpack Encore!');
